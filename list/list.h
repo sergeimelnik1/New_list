9c9ab49956aeccb2829fc1677d82d1b43d1d1df3
sergeimelnik1 <sergeimelnik-1@yandex.ru>
2016-10-29 10:02:04 +0300

    make new code

--- list/list.h
+++ list/list.h
@@ -1,17 +1,17 @@
-
-struct list * init(int a);
-struct list * add_elem_end(list *lst, int number);
-struct list * addToStart(list *lst, int number);
-struct list * delete_elem(list *lst, int n);
-struct list * deletehead(list *root);
-void listprint(list *lst);
-struct list * swap(struct list *lst1, struct list *lst2, struct list *head);
-struct list *search(struct list *list, int find_num);
-void * addToMiddle(list *lst, int after, int number);
-
-struct list {
-    int field; // поле данных
-    struct list *next; // указатель на следующий элемент
-    struct list *prev; // указатель на предыдущий элемент
-};
 using namespace std;
+struct List{
+	int data;
+	struct List *next;
+	struct List *prev;
+};
+
+struct List *createlist(int data);
+void printlist(struct List *first);
+struct List * addTobegin(List*lst, int number);
+struct List * addToend(List*lst, int number);
+void addTomiddle(List**lst, int number, int after);
+void deleteElem(List**lst,int number);
+struct List *deletehead(List*root);
+struct List *deletend(List*root);
+struct List * search(List*node, int val);
+void sort(List**head);
